3705c4e7ea757241e0fae578d7709beb10a55290
Joe Garlick <joe.garlick@discsystems.co.uk>
2020-04-10 00:19:03 +0100

    Test function

--- UpdateModules/Node1Updates.php
+++ UpdateModules/Node1Updates.php
@@ -3,17 +3,18 @@ class UpdateModule_Node1Update implements UpdateModule
 {
     public $Node = "node1";
 
-    public function PreRun($GitFolder)
+    public function PreRun($ModuleHelper, $GitFolder)
     {
         echo "prerun" . PHP_EOL;
+        $ModuleHelper::TestFunction();
     }
 
-    public function Run($GitFolder)
+    public function Run($ModuleHelper, $GitFolder)
     {
         echo "run" . PHP_EOL;
     }
 
-    public function PostRun($GitFolder)
+    public function PostRun($ModuleHelper, $GitFolder)
     {
         echo "postrun" . PHP_EOL;
     }
