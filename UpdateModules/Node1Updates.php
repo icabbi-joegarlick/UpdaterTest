5d91d94145673247398412827bddde94fb4d778a
Joe Garlick <joe.garlick@discsystems.co.uk>
2020-05-28 10:30:26 +0100

    Added Debugging for master branch

--- UpdateModules/Node1Updates.php
+++ UpdateModules/Node1Updates.php
@@ -5,16 +5,16 @@ class UpdateModule_Node1Update implements UpdateModule
 
     public function PreRun($ModuleHelper, $GitFolder)
     {
-        echo "prerun" . PHP_EOL;
+        echo "prerun - master" . PHP_EOL;
     }
 
     public function Run($ModuleHelper, $GitFolder)
     {
-        echo "run" . PHP_EOL;
+        echo "run - master" . PHP_EOL;
     }
 
     public function PostRun($ModuleHelper, $GitFolder)
     {
-        echo "postrun" . PHP_EOL;
+        echo "postrun - master" . PHP_EOL;
     }
 }
